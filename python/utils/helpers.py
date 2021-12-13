de2e07e1106cb0652a3846faee37f8f9141aef51
Gonçalo Antunes <xibitix@hotmail.com>
2021-12-13 22:00:16 +0000

    Add helpers to parse input matrix and print

--- python/utils/helpers.py
+++ python/utils/helpers.py
@@ -18,3 +18,38 @@ def get_lines(f, regex=None, parse_pattern=None, strip=True):
     return [re.compile(regex).findall(line.strip() if strip else line)[0] for line in f.readlines()]
 
   return [line.strip() if strip else line for line in f.readlines()]
+
+
+def get_matrix(f, cast=None, start=(0, 0), y_start_at_top=True, strip=True):
+  start_x, start_y = start
+  m = {}
+
+  lines = f.readlines()
+  if not y_start_at_top:
+    lines = lines[::-1]
+
+  for i, line in enumerate(lines):
+    if strip:
+      line = line.strip()
+    y = start_y + i
+    for j, c in enumerate(line):
+      x = start_x + j
+      m[(x, y)] = c if cast is None else cast(c)
+
+  return m
+
+
+def print_matrix(matrix, default_val='.', mapping=None):
+  if not matrix:
+    return
+
+  min_x = int(min(matrix.keys(), key=lambda p: p[0])[0])
+  max_x = int(max(matrix.keys(), key=lambda p: p[0])[0])
+  min_y = int(min(matrix.keys(), key=lambda p: p[1])[1])
+  max_y = int(max(matrix.keys(), key=lambda p: p[1])[1])
+  for y in range(min_y, max_y + 1):
+    line = ''
+    for x in range(min_x, max_x + 1):
+      val = matrix.get((x, y), default_val)
+      line += str(mapping.get(val, val) if mapping is not None else val)
+    print(line)
