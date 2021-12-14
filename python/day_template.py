986f9a66d5ee3a21ad98ac7c1205fba93ef47559
Gonçalo Antunes <xibitix@hotmail.com>
2021-12-14 05:52:29 +0000

    Update template

--- python/day_template.py
+++ python/day_template.py
@@ -20,19 +20,28 @@ def parse_line(line):
 
 
 try:
+  fin = aoc.get_input(DAY, example=DEBUG)
   ints = get_ints(fin)
-finally:
-  fin = aoc.get_input(DAY, example=DEBUG)
+except:
+  pass
+
 try:
+  fin = aoc.get_input(DAY, example=DEBUG)
   lines = get_lines(fin, parse_pattern=None)
-  lines = [parse_line(line) for line in lines]
+except:
+  lines = None
 finally:
-  fin = aoc.get_input(DAY, example=DEBUG)
+  if lines is not None:
+    plines = [parse_line(line) for line in lines]
+
 try:
+  fin = aoc.get_input(DAY, example=DEBUG)
   g = get_matrix(fin)
   g = get_matrix(fin, cast=int)
-finally:
-  fin = aoc.get_input(DAY, example=DEBUG)
+except:
+  pass
+
+fin = aoc.get_input(DAY, example=DEBUG)
 
 ###
 
@@ -41,7 +50,7 @@ ans = 0
 ans = ans
 aoc.print_answer(ans, 1)
 if not DEBUG:
-  input("Submit ? (Ctrl-c to cancel)")
+  input("Submit part 1 ? (Ctrl-c to cancel)")
   aoc.submit_answer(ans, 1, DAY, YEAR)
 
 ###
@@ -51,5 +60,5 @@ ans = 0
 ans = ans
 aoc.print_answer(ans, 2)
 if not DEBUG:
-  input("Submit ? (Ctrl-c to cancel)")
+  input("Submit part 2 ? (Ctrl-c to cancel)")
   aoc.submit_answer(ans, 2, DAY, YEAR)
