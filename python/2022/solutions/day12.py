cc2fb2db48a77be319a7b9bce4578deb34d05fbd
Gonçalo Antunes <xibitix@hotmail.com>
2022-12-14 06:47:56 +0000

    Improve day 12 solution to use BFS for part 1 and Invert Graph + BFS for part 2 instead of Dijkstra

--- python/2022/solutions/day12.py
+++ python/2022/solutions/day12.py
@@ -1,5 +1,5 @@
 from utils import aoc
-from utils.algorithms import dijkstra_cost
+from utils.algorithms import bfs, invert_graph
 from utils.helpers import get_dgrid, graph_from_dgrid
 
 YEAR, DAY = 2022, 12
@@ -23,8 +23,8 @@ for pos, height in heightmap.items():
 
 g = graph_from_dgrid(heightmap, weighted=False, edge_filter=lambda s, t, sw, tw: tw - sw <= 1)
 
-ans = dijkstra_cost(g, source, target, weighted=False)
+ans = bfs(g, start=source, ends=[target])
 aoc.submit_handler(ans, part=1, day=DAY, year=YEAR, skip_confirmation=True)
 
-ans = min(dijkstra_cost(g, start, target, weighted=False) for start in start_elevations)
+ans = bfs(invert_graph(g, weighted=False), start=target, ends=start_elevations)
 aoc.submit_handler(ans, part=2, day=DAY, year=YEAR, skip_confirmation=True)
